40a696c44264b75f9cb0c125846b4eefd0d3c019
nyidaavuesther-lgtm <nyidaavuesther@gmail.com>
2026-06-10 14:39:15 -0800

    Add files via upload

---
+++ Work2.java
@@ -0,0 +1,10 @@
+public class Work2 {
+    static void main(String[] args) {
+        System.out.println("X                        X");
+        System.out.println("X                        X");
+        System.out.println("X       XXXXXXXXXX       X");
+        System.out.println("XXXXX   X        X   XXXXX");
+        System.out.println("X   X   X        X   X   X");
+        System.out.println("X   X   X        X   X   X");
+    }
+}
---
+++ Work3.java
@@ -0,0 +1,11 @@
+public class Work3 {
+    static void main(String [] args){
+        System.out.println("      T");
+        System.out.println("     TTT");
+        System.out.println("    TTTTT");
+        System.out.println("   TTTTTTT");
+        System.out.println("  TTTTTTTTT");
+        System.out.println(" TTTTTTTTTTT");
+        System.out.println("TTTTTTTTTTTTT");
+    }
+}
